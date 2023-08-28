78709a226acdd676e5d2a71d8c45d9d90be5d4d6
OSUMed <62990411+OSUMed@users.noreply.github.com>
2023-08-27 22:25:03 -0700

    make skeleton for all endpoints

--- src/main/java/com/example/assignment_9/web/RecipeController.java
+++ src/main/java/com/example/assignment_9/web/RecipeController.java
@@ -5,9 +5,34 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RecipeController {
-	
+
 	@GetMapping("")
 	public String getTest() {
 		return "This is test endpoint";
+	}
+
+	@GetMapping("/gluten-free")
+	public String getGlutenFree() {
+		return "This is gluten-free\" endpoint";
+	}
+
+	@GetMapping("/vegan")
+	public String getVegan() {
+		return "This is vegan endpoint";
+	}
+
+	@GetMapping("/vegan-and-gluten-free")
+	public String getVeganAndGluten() {
+		return "This is vegan-and-gluten-free endpoint";
+	}
+
+	@GetMapping("/vegetarian")
+	public String getVege() {
+		return "This is vegetarian endpoint";
+	}
+
+	@GetMapping("/all-recipes")
+	public String allRecipes() {
+		return "This is all-recipes endpoint";
 	}
 }
