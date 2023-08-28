f1940d59038420aec192006b5130ef00e7da81ce
OSUMed <62990411+OSUMed@users.noreply.github.com>
2023-08-28 00:20:56 -0700

    add working read CSV Reader with 12 items/row

--- src/main/java/com/example/assignment_9/services/FileService.java
+++ src/main/java/com/example/assignment_9/services/FileService.java
@@ -1,19 +1,48 @@
 package com.example.assignment_9.services;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
+@Service
 public class FileService {
-	
+
 	@Value("${file.name}")
 	private String fileName;
-	
-	public List<String> processFile(){
-		List<String> fileContent = new ArrayList<>();
-		
-		
+
+	public List<CSVRecord> processFile() throws IOException {
+		// TODO Auto-generated method stub
+
+		System.out.println("inside process file " + fileName);
+		List<CSVRecord> fileContent = new ArrayList<>();
+//		Reader in = new FileReader("recipes.txt");
+		Reader in = new FileReader(fileName);
+		Iterable<CSVRecord> records = CSVFormat.DEFAULT.withHeader("Cooking Minutes", "Dairy Free", "Gluten Free",
+				"Instructions", "Preparation Minutes", "Price Per Serving", "Ready In Minutes", "Servings",
+				"Spoonacular Score", "Title", "Vegan", "Vegetarian").withIgnoreSurroundingSpaces().withEscape('\\')
+				.parse(in);
+		Boolean lineOne = true;
+		Integer counter = 0;
+		for (CSVRecord record : records) {
+			if (lineOne) {
+				lineOne = false;
+				continue;
+			}
+
+			counter++;
+			fileContent.add(record);
+			System.out.println(record.size());
+		}
+
+		System.out.println("counter is: " + counter);
 		return fileContent;
 	}
+
 }
--- src/main/java/com/example/assignment_9/web/RecipeController.java
+++ src/main/java/com/example/assignment_9/web/RecipeController.java
@@ -1,13 +1,25 @@
 package com.example.assignment_9.web;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.assignment_9.services.FileService;
+
 @RestController
 public class RecipeController {
+	
+	
+	@Autowired
+	private FileService fileService;
 
 	@GetMapping("")
 	public String getTest() {
+
 		return "This is test endpoint";
 	}
 
@@ -18,6 +30,13 @@ public class RecipeController {
 
 	@GetMapping("/vegan")
 	public String getVegan() {
+		try {
+			List<CSVRecord> fileContent = fileService.processFile();
+//			System.out.println(fileContent);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return "This is vegan endpoint";
 	}
 
